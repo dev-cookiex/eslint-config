78310cbe642e6be59e601222b5b9e561bf968012
tony-tsx <tony.js@zoho.eu>
2020-07-13 14:18:27 -0300

    configure license fields

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+// Copyright (c) 2020 CookieX
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
 import config from './.eslintrc.json'
 
 export = config
