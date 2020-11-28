68285e6ce768695abc5e9eb43812450028ed3e29
Tony <tony.js@zoho.eu>
2020-11-28 14:34:38 -0300

    feat: last build

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 "use strict";
+// Copyright (c) 2020 CookieX
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
